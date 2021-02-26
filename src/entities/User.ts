946c5e3e9ac81abd6c46324562ae895fe2d96a0d
Vivek Joy <vivekjne@gmail.com>
2021-02-26 20:58:57 +0530

    added typeorm config,User entity and register route

---
+++ src/entities/User.ts
@@ -0,0 +1,54 @@
+import { IsEmail, Length, Min } from "class-validator";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  BeforeInsert,
+} from "typeorm";
+import bcrypt from "bcrypt";
+import { classToPlain, Exclude } from "class-transformer";
+
+@Entity("users")
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
+
+  @Index()
+  @Length(3, 255)
+  @Column({ unique: true })
+  username: string;
+
+  @Exclude()
+  @Column()
+  @Length(6, 255)
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @CreateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
+}
---
+++ src/middleware/trim.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const exceptions = ["password"];
+
+  Object.keys(req.body).forEach((key) => {
+    if (!exceptions.includes(key) && typeof req.body[key] === "string") {
+      req.body[key] = req.body[key].trim();
+    }
+  });
+  next();
+};
